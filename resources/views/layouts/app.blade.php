717063bc5f4592841b957aaccbe21c4c11f862ef
Shuvo <shuvot01794@gmail.com>
2026-04-11 20:01:21 +0600

    refactor: extract car item card into reusable component and remove footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,16 +1,9 @@
-@props(['title' => '', 'footerLinks' => ''])
+@props(['title' => ''])
 
 <x-base-layout :$title>
 
     <x-layouts.header />
 
     {{ $slot }}
-
-    <footer>
-        <a href="#">link 1</a>
-        <a href="#">link 2</a>
-       {{ $footerLinks }}
-    </footer>
-    
 
 </x-base-layout>
