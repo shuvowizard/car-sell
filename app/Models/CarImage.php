5c0cd1f7ff44cfd9d00c2b0d657194df25a18f3d
Shuvo <shuvot01794@gmail.com>
2026-05-09 19:21:59 +0600

    refactor: rename CarImages model to CarImage

--- app/Models/CarImage.php
+++ app/Models/CarImage.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class CarImages extends Model
+class CarImage extends Model
 {
     use HasFactory;
 
